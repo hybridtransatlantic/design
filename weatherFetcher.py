3bbb080ff64f8bd439a4ccab2ff265863f040b25
Hybrid Transatlantic <hybridtransatlantic@gmail.com>
2017-12-15 17:53:21 -0500

    xml to json

--- weatherFetcher.py
+++ weatherFetcher.py
@@ -1,5 +1,7 @@
 import requests,json,math
-import xml.etree.ElementTree as ET
+from xmljson import badgerfish as bf
+from xml.etree.ElementTree import fromstring
+
 
 
 def waypoints(lat1, lon1, lat2, lon2, N):
@@ -65,7 +67,7 @@ def getKeys():
     return json.load(open('/home/eli/PycharmProjects/design/API_keys.json'))['keys']
 
 
-def fetchGtsreatCircleWeather(wypts, weatherKey):
+def fetchGreatCircleWeather(wypts, weatherKey):
     file = open('weatherData', 'a')
     numPts = len(wypts['lats'])
     for i in range(0,numPts):
@@ -77,9 +79,45 @@ def fetchGtsreatCircleWeather(wypts, weatherKey):
 
     file.close()
 
+
+def fetchGreatCircleWeatherToDict(wypts, weatherKey):
+    output = {}
+    numPts = len(wypts['lats'])
+    for i in range(0, numPts):
+        lat = wypts['lats'][i]
+        lon = wypts['lons'][i]
+        URL = "http://api.worldweatheronline.com/premium/v1/marine.ashx?key=" + weatherKey + "&format=json&q=" + str(
+            lat) + "," + str(lon)
+        response = requests.post(URL)
+        key = str(lat) + ',' + str(lon)
+        output[key] = textToDict(response.text)
+
+    return output
+
+
+def textToDict(text):
+    data = dict(bf.data(fromstring(text)))
+    nestedData = data['data']['weather'][0]
+    output = {}
+    date = nestedData['date']['$']
+    tmp = {}
+    for i in nestedData['hourly']:
+        tmp[str(i['time']['$']).zfill(4)] = {'WindGustKmph':i['WindGustKmph']['$'],'pressure':i['pressure']['$'],'winddirDegree':i['winddirDegree']['$'],'windspeedKmph':i['windspeedKmph']['$']}
+    output[date] = tmp
+    return output
+
+
 keys = getKeys()
 weatherKey = keys['weather']
-wypts = waypoints(42.836329, -70.973406,52.642808, -9.469758, 100.0)
-print weatherCall(-40.5, 47, weatherKey)
+wypts = waypoints(42.836329, -70.973406, 52.642808, -9.469758, 100.0)
+result = fetchGreatCircleWeatherToDict(wypts, weatherKey)
+jsonOut = json.dumps(result)
+f = open('test.json','w')
+f.write(jsonOut)
+f.close()
+
+print result
+#response = weatherCall(-40.5, 47, weatherKey)
+#print textToDict(response)
 
 
